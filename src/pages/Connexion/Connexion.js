33d620b51bcfe4c1f8b72506d4912789c6219180
Mouhamadou <mouhamadoudioufsn@gmail.com>
2021-02-20 18:54:30 +0000

    Update api url for production

--- src/pages/Connexion/Connexion.js
+++ src/pages/Connexion/Connexion.js
@@ -34,7 +34,8 @@ const useStyles = makeStyles((theme) => ({
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundImage:
+      'url(https://images.unsplash.com/photo-1471174617910-3e9c04f58ff5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light'
